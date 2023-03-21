7608e27e47d7cf94d446c899119c7a20fc955ce7
Shiryu180 <facuarrat@hotmail.com>
2023-03-21 13:00:03 -0300

    Update TP.java
    out de nombre pepito

--- TP.java
+++ TP.java
@@ -2,5 +2,7 @@ public class TP {
     public static void main(String[] args) {
         System.out.println("hola estoy colaborando en github");
         System.out.println("otra modificacion");
+        var nombre = "pepito";
+        System.out.println(nombre);
     }
 }
